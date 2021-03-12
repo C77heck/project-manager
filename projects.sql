f1c13d096b05b1cb9e5fae2c9b74b8951eefee2d
sug-madic <zcsilleri@gmail.com>
2021-03-12 03:33:31 +0100

    swapped sql file out

--- projects.sql
+++ projects.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 12, 2021 at 03:07 AM
+-- Generation Time: Mar 12, 2021 at 03:29 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -38,8 +38,15 @@ CREATE TABLE `owners` (
 --
 
 INSERT INTO `owners` (`id`, `name`, `email`) VALUES
-(1, 'Test Owner', 'email@gmail.com');
-
+(1, 'Test Owner', 'email@gmail.com'),
+(30, 'Test Owner', 'new@email.hu'),
+(31, 'just another owner', 'another@email.io'),
+(32, 'Test Owner', 'test@owner.com'),
+(33, 'Test Owner', 'test@test.io'),
+(34, 'Test Owner', 'best@owner.com'),
+(35, 'one more', 'one@more.in'),
+(36, 'Test Owner', 'owner@test.io'),
+(37, 'Test Owner', 'james@bohner.io');
 
 -- --------------------------------------------------------
 
@@ -60,7 +67,15 @@ CREATE TABLE `projects` (
 INSERT INTO `projects` (`id`, `title`, `description`) VALUES
 (1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Phasellus porttitor molestie erat. Mauris vulputate at arcu at elementum. Etiam faucibus varius porta. Donec in magna in lorem congue varius vel in elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sit amet diam molestie, elementum mi dapibus, egestas libero. Duis sem lectus, laoreet quis semper nec, mattis eget ex. Integer ac lobortis tortor. Sed at varius ipsum. Cras eget lacus non turpis egestas faucibus. Morbi vel iaculis felis, sed lobortis nunc. Donec placerat magna id quam vestibulum accumsan.'),
 (2, 'Pellentesque bibendum vehicula sapien, a molestie velit pharetra nec.', 'Donec eget neque nec eros interdum porta. Quisque quis tempor massa, et pellentesque dolor. Phasellus accumsan velit ut porttitor hendrerit. Donec sodales diam id vehicula aliquam. Praesent ut lorem quis neque aliquet tincidunt et a eros. Pellentesque non vulputate elit. Sed auctor neque id velit porttitor, vitae interdum turpis blandit. Nulla cursus eros accumsan justo aliquet, ut egestas diam mollis. Sed gravida orci nisl, quis tristique dui consectetur eget. Donec at nisl ac libero imperdiet tempor et ac diam. Phasellus vitae nibh sed tortor fringilla placerat id at lacus.'),
-(3, 'Vestibulum sapien metus, feugiat non nunc sed, laoreet luctus dolor.', 'Vestibulum nibh urna, rutrum sit amet sem ut, rhoncus tristique lectus. Etiam laoreet efficitur tincidunt. In hac habitasse platea dictumst. Integer fringilla mi quam. Cras enim orci, pharetra eu blandit id, tincidunt at justo. Nunc sed leo a sapien laoreet pretium eleifend eget purus. Sed in sapien quis diam posuere pharetra quis vel sapien. Sed sed aliquet neque, in rhoncus ex. Aliquam posuere euismod magna, ut consequat nulla placerat vitae. Aenean fringilla, tellus sed aliquet molestie, nisi urna aliquet quam, et tincidunt mauris quam vitae ligula. Nullam tristique mattis pretium. Pellentesque vel ultricies elit. Maecenas elementum magna dignissim ex molestie, quis fringilla leo venenatis. Donec ut velit eget ex commodo volutpat vitae non arcu. Aenean finibus ullamcorper justo, nec ullamcorper magna ullamcorper nec.');
+(3, 'Vestibulum sapien metus, feugiat non nunc sed, laoreet luctus dolor.', 'Vestibulum nibh urna, rutrum sit amet sem ut, rhoncus tristique lectus. Etiam laoreet efficitur tincidunt. In hac habitasse platea dictumst. Integer fringilla mi quam. Cras enim orci, pharetra eu blandit id, tincidunt at justo. Nunc sed leo a sapien laoreet pretium eleifend eget purus. Sed in sapien quis diam posuere pharetra quis vel sapien. Sed sed aliquet neque, in rhoncus ex. Aliquam posuere euismod magna, ut consequat nulla placerat vitae. Aenean fringilla, tellus sed aliquet molestie, nisi urna aliquet quam, et tincidunt mauris quam vitae ligula. Nullam tristique mattis pretium. Pellentesque vel ultricies elit. Maecenas elementum magna dignissim ex molestie, quis fringilla leo venenatis. Donec ut velit eget ex commodo volutpat vitae non arcu. Aenean finibus ullamcorper justo, nec ullamcorper magna ullamcorper nec.'),
+(140, 'i ne accusam pertinacia, luptatum oporteat sea an.', '\r\nQuo et ferri regione alterum, graeci accusamus assueverit mea ad, mentitum insolens et mel. Eros scripta ea quo, tacimates accusata ne duo. Usu at congue lucilius quaerendum, in ferri malorum nostrum vel. Congue reprehendunt sed ne, sed amet intellegam no. Duis minim praesent nam no, lorem iriure qualisque eos an.\r\n'),
+(141, 'et mel. Eros scripta ea quo, tacimates accusa', 'Lorem ipsum dolor sit amet, et platonem sententiae interesset quo. Sea ne amet postea appareat, fugit melius cu vim. Sumo omittam an pri, ut eum invidunt dignissim definiebas, stet putant molestiae qui an. Pri ne accusam pertinacia, luptatum oporteat sea an.'),
+(142, 'eloquentiam ex. Duo quot tincidunt efficiantur et. Lab', 'ostly a part of a Latin text by the classical author and philosopher Cicero. Its words and letters have been changed by addition or removal, so to deliberately render its content nonsensical; it\'s not genuine, correct, or comprehensible Latin anymore. While lorem ipsum\'s still resembles classical Latin, it actually has no meaning whatsoever. As Cicero\'s text doesn\'t contain the letters K, W, or Z, alien to latin, these, and others are often inserted randomly to mimic the typographic appearence of E'),
+(143, ' dissentiet. Graece dissentias nec id, eos cu aliquid persequeris.', 'Lorem ipsum dolor sit amet, malorum mediocrem vel ea. Ad vim pertinacia intellegam, ne modus liber offendit vix. Sed ipsum voluptaria definiebas ei. Mel ei perfecto praesent definiebas, id sale cetero reprehendunt mel. Nec sanctus sensibus ne, id eam odio assenti'),
+(144, 'er honestatis ullamcorper. An eius imperdiet nec, eum dicit postulant eloquentiam', 'Lorem ipsum dolor sit amet, malorum mediocrem vel ea. Ad vim pertinacia intellegam, ne modus liber offendit vix. Sed ipsum voluptaria definiebas ei. Mel ei perfecto praesent definiebas, id sale cetero reprehendunt mel. Nec sanctus sensibus ne, id eam odio assentior\r\nUsu vide habeo mutat ad, dicit vocent vim'),
+(145, ' aliquid persequeris. mo eirmod. Affert partem no eum, vix in viderer hones', 'Usu vide habeo mutat ad, dicit vocent vim ad, eum ne su dissentiet. Graece dissentias nec id, eos cu aliquid persequeris.\r\nmo eirmod. Affert partem no eum, vix in viderer honestatis ullamcorper. An eius imperdiet nec, eum dicit postulant eloquentiam ex. Duo quot tincidunt efficiantur et. Labore blandit detracto te sea, sed congue utroque moderatius i'),
+(146, 'aliquid scriptorem cum an. Ne vim sumo facete. Pri at scaevola invenire.', 'Lorem ipsum dolor sit amet, zril saperet consequuntur cu vis, usu no semper evertitur. Te mea ludus nobis mentitum, summo dignissim eu duo. At aliquid sanctus vix, in usu tation doming. Id usu oportere pertinacia vituperata, an mea mollis conceptam, aliquid scriptorem cum an. Ne vim sumo facete. Pri at scaevola invenire.\r\n'),
+(147, 'probo dicit no. Pri eirmod verterem gloriatur ea.', 'Lorem ipsum dolor sit amet, zril saperet consequuntur cu vis, usu no semper evertitur. Te mea ludus nobis mentitum, summo dignissim eu duo. At aliquid sanctus vix, in usu tation doming. Id usu oportere pertinacia vituperata, an mea mollis conceptam, aliquid scriptorem cum an. Ne vim sumo facete. Pri at scaevola invenire.\r\n\r\nHas at vide porro alterum, his an modo invenire tractatos. Ei equidem oporteat ius. Pri probo dicit no. Pri eirmod verterem gloriatur ea.\r\n');
 
 -- --------------------------------------------------------
 
@@ -80,7 +95,15 @@ CREATE TABLE `project_owner_pivot` (
 INSERT INTO `project_owner_pivot` (`project_id`, `owner_id`) VALUES
 (2, 1),
 (3, 1),
-(1, 1);
+(1, 1),
+(140, 30),
+(141, 31),
+(142, 32),
+(143, 33),
+(144, 34),
+(145, 35),
+(146, 36),
+(147, 37);
 
 -- --------------------------------------------------------
 
@@ -99,9 +122,16 @@ CREATE TABLE `project_status_pivot` (
 
 INSERT INTO `project_status_pivot` (`project_id`, `status_id`) VALUES
 (1, 1),
+(143, 1),
+(144, 1),
 (2, 2),
-(3, 3);
-
+(140, 2),
+(145, 2),
+(146, 2),
+(147, 2),
+(3, 3),
+(141, 3),
+(142, 3);
 
 -- --------------------------------------------------------
 
@@ -170,13 +200,13 @@ ALTER TABLE `statuses`
 -- AUTO_INCREMENT for table `owners`
 --
 ALTER TABLE `owners`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=140;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=148;
 
 --
 -- AUTO_INCREMENT for table `statuses`
